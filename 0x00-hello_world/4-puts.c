1c44d0bb98b478bfbb6470091671e5859d06e643
Juan Diego <905@holbertonschool.com>
2019-06-19 19:51:34 +0000

    correcting the format of 4 file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 /**
  *Print the string specified
+ *
  *with the command puts
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
- return (0);
+return (0);
 }
