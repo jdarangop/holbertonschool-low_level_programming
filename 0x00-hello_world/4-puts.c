8e8dcfc53697958bcda39e4c21e94dcbace80b53
Juan Diego <905@holbertonschool.com>
2019-06-19 19:46:00 +0000

    correction of file 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
-/* Print the string specified */
+/**
+ *Print the string specified
+ *with the command puts
+ */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return 0;
+puts("\"Programming is like building a multilingual puzzle");
+ return (0);
 }
