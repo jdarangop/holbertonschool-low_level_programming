9b1cb1a6f1838824d4396c4e900dd60a29ea19da
Juan Diego <905@holbertonschool.com>
2019-06-19 20:46:20 +0000

    adding 6 file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char:  byte(s)", sizeof(char));
+printf("Size of an int:  byte(s)", sizeof(int));
+printf("Size of a long int:  byte(s)", sizeof(long int));
+printf("Size of a long long int:  byte(s)", sizeof(long long int);
+printf("Size of a float:  byte(s)", );
+return (0);
+}
+
