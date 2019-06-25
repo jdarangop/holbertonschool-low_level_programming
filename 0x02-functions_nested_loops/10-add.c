7344f64d35825aef795fd4a5425b995c035f81fc
Juan Diego <905@holbertonschool.com>
2019-06-25 16:57:47 +0000

    adding 10 and 11 files

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * add - Sum the two numbers.
+ *
+ * @a: A number.
+ *
+ * @b: A number.
+ *
+ * Return: The sum of a plus b.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
