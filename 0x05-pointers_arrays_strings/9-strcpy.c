c46f54bb3c0392f390765a6a56b8be482ad24c93
Juan Diego <905@holbertonschool.com>
2019-07-02 21:32:40 +0000

    Adding 9 task

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - Print the same string.
+ * @dest: array;
+ * @src: string;.
+ *
+ * Return: The array.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int j, i = 0;
+
+	while (*(src + i) != '\0')
+	{
+		i++;
+	}
+
+	for (j = 0; j <= i; j++)
+	{
+		*(dest + j) = *(src + j);
+	}
+	*(dest + j) = '\0';
+	return (dest);
+}
