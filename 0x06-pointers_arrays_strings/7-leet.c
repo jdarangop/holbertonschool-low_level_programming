73e46ed9efac7d3df3eed91a881a0ba1d749ac21
jdarangop <905@holbertonschool.com>
2019-07-04 13:02:46 +0000

    Adding 7 task

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,3 +1,22 @@
 #include "holberton.h"
 
+char *leet(char *s)
+{
+	char array[5][2] = {{'a', 'A'}, {'e', 'E'},
+		{'o', 'O'}, {'t', 'T'}, {'l', 'L'}};
+	char keys[5] = {'4', '3', '0', '7', '1'};
 
+	int i = 0, j;
+
+	while (s[i])
+	{
+		for (j = 0; j < 5; j++)
+		{	
+			if (array[j][0] == s[i] || array[j][1] == s[i])
+				s[i] = keys[j];
+		}
+		i++;
+	}
+	return (s);
+}
+			
