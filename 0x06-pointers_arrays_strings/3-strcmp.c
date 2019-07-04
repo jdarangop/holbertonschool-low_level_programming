86986b956d39b4ece77c489e67e2636c6bcf4072
Juan Diego <905@holbertonschool.com>
2019-07-04 20:30:31 +0000

    fixed 3 task and adding 5 and 6 tasks

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,44 +2,26 @@
 
 
 /**
- *_strlen - Get the len.
- * @s: string.
- * Return: len of array.
- */
-int _strlen(char *s)
-{
-	int contador = 0;
-
-	while (*(s + contador) != '\0')
-	{
-		contador++;
-	}
-	return (contador);
-}
-
-/**
   * _strcmp - Like strcmp.
   * @s1: string.
   * @s2: string.
-  * Return: nothing.
+  * Return: int.
   */
 int _strcmp(char *s1, char *s2)
 {
-	int size1 = _strlen(s1);
-	int size2 = _strlen(s2);
+	int i = 0;
 
-	if (size1 > size2)
+
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		return (*s1 - *s2);
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+
+		return (0);
 	}
-	else if (size1 == size2)
-	{
-		return (*s1 - *s2);
-	}
-	else
-	{
-		return (*s1 - *s2);
-	}
+	return (0);
 }
 
 
---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - Pass to upper the string.
+ * @str: String.
+ * Return: String upper.
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] -= 32;
+		}
+	}
+	return (str);
+}
