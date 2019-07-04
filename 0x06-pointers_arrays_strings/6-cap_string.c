e57e820dcce02c73b46c7755fa757823076685c5
Juan Diego <905@holbertonschool.com>
2019-07-04 21:54:20 +0000

    Adding 6 task

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,20 +1,35 @@
 #include "holberton.h"
 
 /**
- * string_toupper - Pass to upper the string.
+ * cap_string - Make some letters capital.
  * @str: String.
  * Return: String upper.
  */
-char *string_toupper(char *str)
+char *cap_string(char *str)
 {
-	int i;
+	int i, num = 0;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
+		if (str[i] == 32 || str[i] == ',' || str[i] == ';' ||
+				str[i] == '.' || str[i] == '!' || str[i] == '?' ||
+				str[i] == '"' || str[i] == '(' || str[i] == ')' ||
+				str[i] == '{' || str[i] == '}' ||
+				str[i] == '\t' || str[i] == '\n')
+		{
+			num = 1;
+		}
+
+		else if (str[i] >= 'a' && str[i] <= 'z' && num == 1)
 		{
 			str[i] -= 32;
+			num = 0;
+		}
+		else
+		{
+			num = 0;
 		}
 	}
+
 	return (str);
 }
