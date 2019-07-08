feb7dece1a646bd56656c0c744f0e8c40e654601
jdarangop <juandiegoap33@gmail.com>
2019-07-08 02:56:08 +0000

    Adding 12 task

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+
+/**
+ * print_number - Print a number like a string.
+ * @n: Number.
+ * Return: void.
+ */
+void print_number(int n)
+{
+	int num;
+	unsigned int m;
+	int i = 1000000000;
+
+	if (n > 0)
+	{
+		while (n / i == 0)
+		{
+			i /= 10;
+		}
+		_putchar(n / i + '0');
+		while (i != 1)
+		{
+			num = (n % i) / (i / 10);
+			_putchar(num + '0');
+			i /= 10;
+		}
+	}
+	else if (n < 0)
+	{
+		m = -1 * n;
+		while (m / i == 0)
+		{
+			i /= 10;
+		}
+		_putchar('-');
+		_putchar(m / i + '0');
+		while (i != 1)
+		{
+			num = (m % i) / (i / 10);
+			_putchar(num + '0');
+			i /= 10;
+		}
+	}
+	else
+	{
+		_putchar(n + '0');
+	}
+}
