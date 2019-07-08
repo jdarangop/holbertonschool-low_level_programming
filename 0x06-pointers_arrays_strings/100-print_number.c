575419efa2689af5e9ce7c91ab31835c804564e3
jdarangop <juandiegoap33@gmail.com>
2019-07-08 02:33:52 +0000

    Fixed Betty Style and some outputs

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -1,28 +1,30 @@
 #include "holberton.h"
 
+/**
+ * print_number - Print a number like a string.
+ * @n: Number.
+ * Return: void.
+ */
 void print_number(int n)
 {
 	int num;
-	int i = 1000000;
+	int i = 1000000000;
 
 	if (n > 0)
 	{
-
 		while (n / i == 0)
 		{
 			i /= 10;
 		}
-
 		_putchar(n / i + '0');
-
-		while ( i != 1)
+		while (i != 1)
 		{
 			num = (n % i) / (i / 10);
 			_putchar(num + '0');
 			i /= 10;
 		}
 	}
-	else if ( n < 0)
+	else if (n < 0)
 	{
 		n *= -1;
 		while (n / i == 0)
@@ -31,8 +33,7 @@ void print_number(int n)
 		}
 		_putchar('-');
 		_putchar(n / i + '0');
-
-		while ( i != 1)
+		while (i != 1)
 		{
 			num = (n % i) / (i / 10);
 			_putchar(num + '0');
