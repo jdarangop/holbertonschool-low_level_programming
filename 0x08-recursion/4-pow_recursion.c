6af956eade32f17fab6a5e48188b55e095c5b678
jdarangop <juandiegoap33@gmail.com>
2019-07-11 02:03:42 +0000

    Adding 4 task

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,7 +9,7 @@ int _pow_recursion(int x, int y)
 {
 	if (y > 0)
 	{
-		return (x * _pow_recursion(x - 1));
+		return (x * _pow_recursion(x, y - 1));
 	}
 	else if (y == 0)
 	{
@@ -19,3 +19,4 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
+}
