62b9cb2e1ae7d348fbc5af95dc00e8891e52379d
jdarangop <juandiegoap33@gmail.com>
2019-07-16 22:52:42 +0000

    Adding 3 task

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - Create a 2D array.
+ * @width: Width of the array.
+ * @height: Height of the array.
+ * Return: 2D array.
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+
+	int **tmp = (int **)malloc(sizeof(int *) * height);
+
+	for (i = 0; i < height; i++)
+	{
+		tmp[i] = (int *)malloc(width * sizeof(int));
+	}
+
+	if (tmp == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < height; i++)
+		{
+			for (j = 0; j < width; j++)
+			{
+				tmp[i][j] = 0;
+			}
+		}
+	}
+
+	return (tmp);
+}
