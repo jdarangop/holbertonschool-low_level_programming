71bbf487b2d3bb891c5d484e8e9fc63ae0703394
jdarangop <juandiegoap33@gmail.com>
2019-07-18 21:03:58 +0000

    Adding first advanced task

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *tmp;
+	unsigned int i;
+
+	if (new_size > old_size)
+	{
+		tmp = malloc(new_size);
+		for (i = 0; i < old_size; i++)
+		{
+			((char *)tmp)[i] = ((char *)ptr)[i];
+		}
+		free(ptr);
+		return (tmp);
+	}
+	else if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	else if (ptr == NULL)
+	{
+		tmp = malloc(new_size);
+		return (tmp);
+	}
+	else if ((new_size == 0) && (ptr != NULL))
+	{
+		free(ptr);
+		return (NULL);
+	}
+	return (NULL);
+}
+
+
+
+
+
