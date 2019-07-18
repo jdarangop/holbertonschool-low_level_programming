15d9985d86a883b0d001dceb138565989e1b3687
jdarangop <juandiegoap33@gmail.com>
2019-07-18 21:27:47 +0000

    Modify the first advanced task

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+  * _realloc - Realloc the space of memory.
+  * @ptr: Old array.
+  * @old_size: Size of the first array.
+  * @new_size: Size of the new array.
+  * Return: New array.
+  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *tmp;
 	unsigned int i;
 
-	if (new_size > old_size)
+	if ((new_size > old_size) || (new_size < old_size))
 	{
 		tmp = malloc(new_size);
 		for (i = 0; i < old_size; i++)
