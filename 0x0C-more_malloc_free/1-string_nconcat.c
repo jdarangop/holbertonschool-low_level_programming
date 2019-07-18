681d42adcda639a3faee5b739e88470979b409c6
jdarangop <juandiegoap33@gmail.com>
2019-07-18 17:20:15 +0000

    Adding 0x0C folder with task from 0 to 3

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * _strlen - Find the lenght of a string.
+  * @s: String.
+  * Return: The lenght, integer.
+  */
+int _strlen(char *s)
+{
+	int count = 0, i = 0;
+
+	while (s[i])
+	{
+		count++;
+		i++;
+	}
+
+	return (count);
+}
+
+/**
+  * string_nconcat - Concat one string and n bytes of the second string.
+  * @s1: String.
+  * @s2: String.
+  * @n: Number of bytes.
+  * Return: Strings concated.
+  */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *tmp;
+	unsigned int i, j;
+	unsigned int count1 = _strlen(s1), count2 = _strlen(s2);
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	tmp = malloc((sizeof(char) * count1) + (sizeof(char) * count2) + 1);
+	if (tmp != NULL)
+	{
+		if (n > count2)
+		{
+			for (i = 0; i < count1; i++)
+			{
+				tmp[i] = s1[i];
+			}
+			for (j = 0; j < count2; j++)
+			{
+				tmp[i] = s2[j];
+				i++;
+			}
+			tmp[i + 1] = '\0';
+			return (tmp);
+		}
+		else
+		{
+			for (i = 0; i < count1; i++)
+			{
+				tmp[i] = s1[i];
+			}
+			for (j = 0; j < n; j++)
+			{
+				tmp[i] = s2[j];
+				i++;
+			}
+			tmp[i + 1] = '\0';
+			return (tmp);
+		}
+	}
+	else
+	{
+		return (NULL);
+	}
+}
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *tmp;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	tmp = malloc(nmemb * size);
+
+	if (tmp == NULL)
+	{
+		free(tmp);
+		return (NULL);
+	}
+
+	for (i = 0; i < nmemb; i++)
+	{
+		tmp[i] = 0;
+	}
+
+	return (tmp);
+}
