8ab0824d88196a1cc366e6e529cba3eab5d3bee8
jdarangop <juandiegoap33@gmail.com>
2019-07-27 17:47:41 +0000

    Fixing 3 task

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,38 +1,21 @@
 #include "variadic_functions.h"
 
-int _strlen(const char *s)
-{
-	int i = 0;
-
-	while (s[i])
-	{
-		i++;
-	}
-	return (i);
-}
 
 void print_all(const char * const format, ...)
 {
-
 	va_list valist;
-	int i = 0;
+	unsigned int i = 0;
 
 	va_start(valist, format);
-
 	while (format == NULL)
-	{
 		return;
-	}
-
 	while (format[i])
 	{
 		char *tmp;
 		int flag = 0;
-
 		switch (format[i])
 		{
 			case 'c':
-				
 				printf("%c", va_arg(valist, int));
 				break;
 			case 'i':
@@ -43,25 +26,21 @@ void print_all(const char * const format, ...)
 				break;
 			case 's':
 				tmp = va_arg(valist, char *);
-				if (tmp != NULL)
-					printf("%s", tmp);
+				if (tmp == NULL)
+				{
+					printf("(nil)");
 					break;
-
-				printf("(nil)");
+				}
+				printf("%s", tmp);
 				break;
 			default:
 				flag = 1;
 				break;
-
 		}
-		if (flag == 0 && i != (_strlen(format) - 1))
-		{
+		if (flag == 0  && i != (strlen(format) - 1))
 			printf(", ");
-		}
 		i++;
 	}
 	printf("\n");
-
 	va_end(valist);
-	
 }
