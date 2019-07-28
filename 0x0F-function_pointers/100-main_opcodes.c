e1eeb5150b9a5014076223e73a39ca3268e1523d
jdarangop <juandiegoap33@gmail.com>
2019-07-28 22:41:22 +0000

    Add advanced task

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+}
