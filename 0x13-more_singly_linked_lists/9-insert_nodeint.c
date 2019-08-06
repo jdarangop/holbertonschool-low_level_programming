588a4a42111aea7c36ea9eb36030e6479cdccbac
jdarangop <juandiegoap33@gmail.com>
2019-08-06 15:59:13 +0000

    Test 9 task without a condition when *head == NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -39,8 +39,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (head == NULL)
 		return (NULL);
 
-	if (*head == NULL && idx == 0)
-		return (NULL);
+	/**
+	*   if (*head == NULL && idx == 0)
+	*	return (NULL);
+	*/
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
