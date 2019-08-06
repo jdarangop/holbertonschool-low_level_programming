fec72411f299026b30051c6bcb6b7632c379350d
jdarangop <juandiegoap33@gmail.com>
2019-08-06 16:12:44 +0000

    Test return new_node when idx == 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -52,7 +52,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		new_node->next = *head;
 		*head = new_node;
-		return (*head);
+		return (new_node);
 	}
 
 	node_prev = get_nodeint_at_index(*head, idx - 1);
