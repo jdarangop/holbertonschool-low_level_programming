e21c6080f307657a6070ae96eb0219e000f81e3c
jdarangop <juandiegoap33@gmail.com>
2019-08-06 15:39:35 +0000

    Fix return in 9 task

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -56,5 +56,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	node_prev = get_nodeint_at_index(*head, idx - 1);
 	new_node->next = node_prev->next;
 	node_prev->next = new_node;
-	return (*head);
+	return (new_node);
 }
