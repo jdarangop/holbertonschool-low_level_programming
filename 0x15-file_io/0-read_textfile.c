c3e8040ac18e9e6794be8f65149cf16529146632
jdarangop <juandiegoap33@gmail.com>
2019-08-13 20:55:53 +0000

    Fix 0 task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -30,7 +30,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-	out = write(1, content, tmp);
+	out = write(STDOUT_FILENO, content, tmp);
 	if (out == -1)
 	{
 		close(fd);
