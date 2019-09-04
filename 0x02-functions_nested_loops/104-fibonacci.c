0b79b8d527eebb5e2fab09c30b5438426c0e53a7
jdarangop <juandiegoap33@gmail.com>
2019-09-04 20:29:55 +0000

    Try with double in 104-fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,51 +1,22 @@
 #include <stdio.h>
 
 /**
-  * main - Print the first 98 fibonacci numbers.
+  * main - Print the first 50 fibonacci numbers.
   *
   * Return: Always 0;
   */
 int main(void)
 {
 	int count;
-	unsigned long int i, j, tmp1, tmp2;
+	long double i, j;
 
 	for (i = 1, j = 2, count = 2; count < 99; i += j, j += i, count += 2)
 	{
-		if (i > 1000000000000000)
-		{
-			tmp1 = i / 1000000000;
-			tmp2 = i % 1000000000;
-			if (tmp2 / 100000000 == 0)
-			{
-				tmp1 *= 10;
-			}
-			if (count != 98)
-				printf("%lu%lu, ", tmp1, tmp2);
-		}
+		printf("%.0Lf, ", i);
+		if (count == 98)
+			printf("%.0Lf\n", j);
 		else
-		{
-			printf("%lu, ", i);
-		}
-
-
-		if (j > 1000000000000000)
-		{
-			tmp1 = j / 1000000000;
-			tmp2 = j % 1000000000;
-			if (tmp2 / 100000000 == 0)
-			{
-				tmp1 *= 10;
-			}
-			if (count == 98)
-				printf("%lu%lu\n", tmp1, tmp2);
-			else
-				printf("%lu%lu, ", tmp1, tmp2);
-		}
-		else
-		{
-			printf("%lu, ", j);
-		}
+			printf("%.0Lf, ", j);
 	}
 	return (0);
 }
