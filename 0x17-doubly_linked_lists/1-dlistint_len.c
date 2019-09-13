b4bfcd1087ea5d218dae1ce1e4dbabbeb39da1b0
jdarangop <juandiegoap33@gmail.com>
2019-09-13 20:32:33 +0000

    Add 0x17 folder

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int counter = 0;
+
+	while (h != NULL)
+	{
+		counter++;
+		h = h->next;
+	}
+	return (counter);
+}
---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t new_node;
+	
+	new_node = malloc(sizeof(dlistint_t));
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+}
