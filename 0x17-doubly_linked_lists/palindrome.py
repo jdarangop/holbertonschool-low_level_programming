9de28323024963dcdd59a145e6b47e654248bbba
jdarangop <juandiegoap33@gmail.com>
2019-09-15 02:39:21 +0000

    Fix second advanced task

--- 0x17-doubly_linked_lists/palindrome.py
+++ 0x17-doubly_linked_lists/palindrome.py
@@ -2,15 +2,18 @@
 
 def reverse(s):
     return s[::-1] 
-
+answers = list()
 def palindrome():
+    maximo = 0
     for i in range(999, 100, -1):
         for j in range(999, 100, -1):
             resultado = str(i * j)
-            if (resultado == reverse(resultado)):
-                print(i)
-                print(j)
-                print(resultado)
-                return(resultado)
+            if ((resultado == reverse(resultado)) and (int(resultado) > maximo)):
+                #print(i)
+                #print(j)
+                #print(resultado)
+                maximo = int(resultado)
+    return(maximo)
 
-palindrome()
+h = palindrome()
+print(h)
