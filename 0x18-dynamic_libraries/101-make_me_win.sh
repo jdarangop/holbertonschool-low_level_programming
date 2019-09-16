4369c409c24a00fbd8fe50348f73cec9a4aec91a
jdarangop <juandiegoap33@gmail.com>
2019-09-16 21:14:13 +0000

    Fix path in the second advanced task

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P ../ https://github.com/jdarangop/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/libcrack.so
+wget -P ../ https://github.com/jdarangop/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/libcrack.so
 export LD_PRELOAD=../libcrack.so
