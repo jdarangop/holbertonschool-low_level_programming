c12e5af4009a0ff1dce5bc66f8cb1ef141c91317
jdarangop <juandiegoap33@gmail.com>
2019-09-21 22:26:50 +0000

    Fix keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 /**
  * main - Contains the leygen for crackme 1.
@@ -9,21 +10,42 @@
 int main(void)
 {
 	char buffer[1024], letra;
-	int suma = 0, i = 0, num;
+	int suma = 0, i = 0, num, num1, num2;
 
-	while (suma < 2673)
+	srand(time(0));
+	num1 = rand();
+	num2 = rand();
+	if (num1 > num2)
 	{
-		srand(i);
-		num = rand() % 13;
-		if (num > 10)
-			letra = 99;
-		else if (num > 5)
-			letra = 66;
-		else
-			letra = 33;
-		suma += letra;
-		buffer[i] = letra;
-		i++;
+		while (suma < 2673)
+		{
+			num = rand() % 13;
+			if (num > 10)
+				letra = 99;
+			else if (num > 5)
+				letra = 66;
+			else
+				letra = 33;
+			suma += letra;
+			buffer[i] = letra;
+			i++;
+		}
+	}
+	else
+	{
+		while (suma < 2673)
+		{
+			num = rand() % 13;
+			if (num > 10)
+				letra = 99;
+			else if (num > 5)
+				letra = 66;
+			else
+				letra = 33;
+			suma += letra;
+			buffer[i] = letra;
+			i++;
+		}
 	}
 	buffer[i] = 2772 - suma;
 	buffer[++i] = '\0';
