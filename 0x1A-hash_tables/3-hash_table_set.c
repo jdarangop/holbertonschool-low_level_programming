25ed604184824b52cebedee13c3dee6b2e83b7e7
jdarangop <juandiegoap33@gmail.com>
2019-10-11 17:28:00 -0500

    Fix 3 task

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,13 +13,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new_node, *current;
 
-	if (strcmp(key, "") == 0 || key == NULL || ht == NULL || value == NULL)
+	if (strcmp(key, "") == 0 || key == NULL || ht == NULL)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
-	new_node->key = (char *)key;
+	new_node->key = strdup((char *)key);
 	new_node->value = strdup((char *)value);
 	new_node->next = NULL;
 	if (ht->array[index] == NULL)
