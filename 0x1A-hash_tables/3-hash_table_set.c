ed5c4312da11bd25c1483c61fa3be23bc236d69c
Holberton School <holbertonschool@Holbertons-iMac-31.local>
2019-10-11 14:50:17 -0500

    Add malloc verifications

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,6 +17,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
 	new_node->key = (char *)key;
 	new_node->value = (char *)value;
 	new_node->next = NULL;
