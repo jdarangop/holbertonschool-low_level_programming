0589bd5e82c4ba2d79a21a08d2b105e8444b2991
jdarangop <juandiegoap33@gmail.com>
2019-10-27 22:31:54 +0000

    Fix: When the list has only 1 or few objects

--- 0x1B-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -12,6 +12,8 @@ void insertion_sort_list(listint_t **list)
 	listint_t *current = *list, *tmp;
 	int var;
 
+	if ((*list)->next == NULL || (*list)->next->next == NULL)
+		return;
 	while (current != NULL)
 	{
 		if (current == *list)
