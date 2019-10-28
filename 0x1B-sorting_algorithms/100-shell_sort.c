caa6ab95eb46d7e2b9696b92b17decde1d5cfcec
jdarangop <juandiegoap33@gmail.com>
2019-10-28 20:26:17 +0000

    Fix: task 4

--- 0x1B-sorting_algorithms/100-shell_sort.c
+++ 0x1B-sorting_algorithms/100-shell_sort.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+#include <stdio.h>
 
 /**
  * shell_sort -  Sorts integers using Shell sort algorithm.
@@ -10,24 +11,22 @@
 
 void shell_sort(int *array, size_t size)
 {
-	int gap, j, tmp, z;
+	int gap, j, tmp;
 	size_t i, k = 1;
-	/*int gaps[500];*/
 
 	if (size < 2)
 		return;
 
 	gap = 1;
-	for (z = 1; z < ((int)size / 3) - 1; z++)
+	while (gap < (int)size)
 	{
 		gap = 3 * gap + 1;
 	}
 
-	while (gap)
+	gap = (gap - 1) / 3;
+	while (gap > 0)
 	{
-		/*gap = gaps[k];*/
-		/*gap = pow((float)3, (float)k) - 1;*/
-		/*gap = gap / 2;*/
+		/*printf("gap: %d\n", gap);*/
 		for (i = gap; i < size; i++)
 		{
 			tmp = array[i];
@@ -38,7 +37,7 @@ void shell_sort(int *array, size_t size)
 			array[j] = tmp;
 		}
 		print_array(array, size);
-		k++;
 		gap = (gap - 1) / 3;
+		k++;
 	}
 }
