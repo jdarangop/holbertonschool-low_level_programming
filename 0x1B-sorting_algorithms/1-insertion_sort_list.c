d1502affbf17acf48ce68c5262b4b9b3cff00c51
jdarangop <juandiegoap33@gmail.com>
2019-10-28 15:39:20 +0000

    Feature: add new condition list == NULL

--- 0x1B-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -12,7 +12,7 @@ void insertion_sort_list(listint_t **list)
 	listint_t *current = *list, *tmp;
 	int var;
 
-	if ((*list)->next == NULL || *list == NULL)
+	if ((*list)->next == NULL || *list == NULL || list == NULL)
 		return;
 	while (current != NULL)
 	{
