f5c97656fe8e8f69e92fef726e0ff994d92c17e9
jdarangop <juandiegoap33@gmail.com>
2019-10-30 04:16:14 +0000

    Feature: add prototypes of cocktail sort in header

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -28,4 +28,6 @@ void shell_sort(int *array, size_t size);
 void partition_caller(int *array, int left, int right, size_t size);
 int partition(int *array, int left, int right, size_t size);
 void counting_sort(int *array, size_t size);
+void swap(listint_t **list, listint_t **node);
+void cocktail_sort_list(listint_t **list);
 #endif
