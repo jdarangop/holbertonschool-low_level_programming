8791b0191f733c7dbbca01a944aba960daa91130
jdarangop <juandiegoap33@gmail.com>
2019-12-05 20:51:56 +0000

    Feature: add task 27

--- 0x1D-binary_trees/binary_trees.h
+++ 0x1D-binary_trees/binary_trees.h
@@ -67,4 +67,5 @@ bst_t *bst_insert(bst_t **tree, int value);
 int binary_tree_is_avl(const binary_tree_t *tree);
 int binary_tree_is_heap(const binary_tree_t *tree);
 bst_t *array_to_bst(int *array, size_t size);
+bst_t *bst_search(const bst_t *tree, int value);
 #endif
