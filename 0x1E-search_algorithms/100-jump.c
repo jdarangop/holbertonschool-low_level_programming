db01e80a8d4fda5f6864853452d745e754336168
jdarangop <juandiegoap33@gmail.com>
2020-01-16 00:06:11 +0000

    Feature: add the display of last range to jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -2,7 +2,7 @@
 #include <math.h>
 
 /**
- * linear_search - Search a value with a linear algorithm
+ * jump_search - Search a value with a jump search algorithm
  * @array: Array to search.
  * @size: Size of the array to search.
  * @value: Value to search.
@@ -12,7 +12,7 @@
 
 int jump_search(int *array, size_t size, int value)
 {
-	int i, step = sqrt(size), j;
+	int i, step = sqrt(size), j, flag = 0;
 
 	for (i = 0; i < (int)size; i += step)
 	{
@@ -27,6 +27,17 @@ int jump_search(int *array, size_t size, int value)
 			}
 		}
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (i + step >= (int)size && flag == 0)
+		{
+			flag = 1;
+			printf("Value found between indexes [%d] and [%d]\n", i, i + step);
+			for (j = i; j < (int)size; j++)
+			{
+				printf("Value checked array[%d] = [%d]\n", j, array[j]);
+				if (array[j] == value)
+					return (j);
+			}
+		}
 	}
 	return (-1);
 }
